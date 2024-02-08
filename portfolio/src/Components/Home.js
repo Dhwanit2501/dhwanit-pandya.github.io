d6d1a8b9558b6e81ab6483fd9f645050039bd487
Dhwanit Pandya <pandyadhwanit25@gmail.com>
2024-02-09 00:34:56 +0530

    Made Nav-Responsive and done with typed-text

--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -1,8 +1,45 @@
+import { useEffect } from "react";
 import NavBar from "./NavBar"
+import Typed from "typed.js";
+import "../Styles/Home.css"
 export default function Home() {
+
+  useEffect(() => {
+    const typed = new Typed(".multiple-text", {
+      strings: [
+        "Frontend Developer^1500",
+        "Cybersecurity Expert^1500",
+        "Student^1500",
+      ],
+      typeSpeed: 100,
+      fadeOut: true,
+      fadeOutClass: "typed-fade-out",
+      fadeOutDelay: 1000,
+      loop: true, // Set loop to true to continuously loop through the strings
+      // onComplete: function (self) {
+      //   // Trigger Typed.js to reset after the animation ends
+      //   self.reset();
+      // },
+    });
+
+    // Cleanup function to destroy Typed instance when component unmounts
+    return () => {
+      typed.destroy();
+    };
+  }, []); 
     return (
       <>
-        <NavBar  />
+        <NavBar />
+        <div className="profiledisplay">
+          <div className="flex-items">
+            <div className="flex-item1">Hello, It's Me</div>
+            <div className="flex-item2">Dhwanit Pandya</div>
+            <div className="flex-item1">
+              And I am a <span className="multiple-text"></span> 
+            </div>
+          </div>
+          <div className="flex-items"></div>
+        </div>
       </>
     );
 }
--- portfolio/src/Components/NavBar.js
+++ portfolio/src/Components/NavBar.js
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import React, { useState, useEffect } from "react";
-import { NavLink,useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,53 +17,67 @@ export default function NavBar() {
     // Update activePage based on the current location's pathname
     setActivePage(location.pathname);
   }, [location.pathname]); // Update when pathname changes
+
+const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <Navbar className="nav">
+      <Navbar className="nav" expand="lg" variant="dark">
         <Container id="cont1">
           <Navbar.Brand href="#home">
             <video width="70" height="70" autoPlay loop muted>
               <source src="./Images/logo4.webm" type="video/webm" />
             </video>
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <NavLink
-              to="/"
-              activeClassName="active" // Apply 'active' class when active
-              className={`nav-item ${
-                activePage === "/" ? "active" : "inactive"
-              }`}
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/About"
-              activeClassName="active" // Apply 'active' class when active
-              className={`nav-item ${
-                activePage === "/About" ? "active" : "inactive"
-              }`}
-            >
-              About
-            </NavLink>
-            <NavLink
-              to="/Work Experience"
-              activeClassName="active" // Apply 'active' class when active
-              className={`nav-item ${
-                activePage === "/Work Experience" ? "active" : "inactive"
-              }`}
-            >
-              Work Experience
-            </NavLink>
-            <NavLink
-              to="/Contact"
-              activeClassName="active" // Apply 'active' class when active
-              className={`nav-item ${
-                activePage === "/Contact" ? "active" : "inactive"
-              }`}
-            >
-              Contact
-            </NavLink>
-          </Nav>
+          <Navbar.Toggle aria-controls="navbarScroll" onClick={handleToggle}>
+            {isOpen ? (
+              <FaTimes className="icon" />
+            ) : (
+              <FaBars className="icon" />
+            )}
+          </Navbar.Toggle>
+          <Navbar.Collapse id="navbarScroll" className={isOpen ? "show" : ""}>
+            <Nav className="me-auto">
+              <NavLink
+                to="/"
+                activeClassName="active" // Apply 'active' class when active
+                className={`nav-item ${
+                  activePage === "/" ? "active" : "inactive"
+                }`}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/About"
+                activeClassName="active" // Apply 'active' class when active
+                className={`nav-item ${
+                  activePage === "/About" ? "active" : "inactive"
+                }`}
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="/Work Experience"
+                activeClassName="active" // Apply 'active' class when active
+                className={`nav-item ${
+                  activePage === "/Work Experience" ? "active" : "inactive"
+                }`}
+              >
+                Work Experience
+              </NavLink>
+              <NavLink
+                to="/Contact"
+                activeClassName="active" // Apply 'active' class when active
+                className={`nav-item ${
+                  activePage === "/Contact" ? "active" : "inactive"
+                }`}
+              >
+                Contact
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
